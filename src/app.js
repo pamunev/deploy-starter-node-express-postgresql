69c708f62228fdc525d2ad0761b56ca740b4e457
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-07-27 18:55:11 -0500

    Something is not working when I do /products in localhost

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 5003;
+
+router.get('/', cors(), (req, res) => {
+  res.json({ message: 'Hello Render hi hey!' })
+});
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `)
+})
+
 const productsRouter = require("./products/products.router");
 const categoriesRouter = require("./categories/categories.router");
 const suppliersRouter = require("./suppliers/suppliers.router");
