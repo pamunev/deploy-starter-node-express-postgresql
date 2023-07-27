288fb261cab4a809a0e39b71613425e0c8551682
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-07-27 18:14:38 -0500

    Preparing deploy

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,16 +1,13 @@
 const productsService = require("./products.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 
-function productExists(req, res, next) {
-  productsService
-    .read(req.params.productId)
-    .then((product) => {
+async function productExists(req, res, next) {
+  const product = await productsService.read(req.params.productId)
       if (product) {
         res.locals.product = product;
         return next();
       }
       next({ status: 404, message: `Product cannot be found.` })
-    })
-    .catch(next);
 }
 
 function read(req, res) {
@@ -18,14 +15,27 @@ function read(req, res) {
   res.json({ data });
 }
 
-function list(req, res, next) {
-  productsService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+async function list(req, res, next) {
+  const data = await productsService.list()
+    res.json({ data })
+}
+
+async function listOutOfStockCount(req, res, next) {
+  res.json({ data: await productsService.listOutOfStockCount() })
+}
+
+async function listPriceSummary(req, res, next) {
+  res.json({ data: await productsService.listPriceSummary() })
+}
+
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() })
 }
 
 module.exports = {
-  read: [productExists, read],
-  list,
+  read: [asyncErrorBoundary(productExists), read],
+  list: asyncErrorBoundary(list),
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
